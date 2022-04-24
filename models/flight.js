e1cb5cbbaee4d88e3911a011bb28762deb09a554
Jordan-Christopher Garcia <jctgarcia20@gmail.com>
2022-04-23 17:11:58 -0700

    Fix Bugs

--- models/flight.js
+++ models/flight.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema;
-	
+
+const destinationSchema = new Schema({
+  airport: {
+    type: String, 
+    enum: ['ATL', 'DFW', 'DEN', 'LAX', 'SAN'],
+    arrival: Date
+  }
+});
+
 const flightSchema = new Schema({
   airline: {
     type: String,
@@ -22,19 +30,11 @@ const flightSchema = new Schema({
     type: Date,
     default: function() {
       let today = new Date();
-      return today.setFullYear(today.getFullYear() + 1)
+      return today.setFullYear(today.getFullYear() + 1);
     }
   },
   destinations: [destinationSchema]
 });
 
-const destinationSchema = new Schema({
-  airport: {
-    type: String, 
-    enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']},
-    arrival: Date
-  }, {
-  timestamps: true
-});
 
 module.exports = mongoose.model('Flight', flightSchema);
--- routes/flights.js
+++ routes/flights.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const flightsCtrl = require('../controllers/movies');
+const flightsCtrl = require('../controllers/flights');
 
 // GET /flights
 router.get('/', flightsCtrl.index);
